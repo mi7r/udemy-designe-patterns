af8eac3f7f5da1d9d478122223eb14cf89a89d31
Radek <michalski.rados@gmail.com>
2019-01-27 18:43:01 +0100

    Builder with internal class.

--- src/main/java/patterns/Main.java
+++ src/main/java/patterns/Main.java
@@ -1,7 +1,16 @@
 package patterns;
 
+import patterns.house.House;
+
 public class Main {
     public static void main(String[] args) {
+        House house = new House.HouseBuilder()
+                .buildWalls("6walls")
+                .buildRoof("1roof")
+                .buildWindows("4windows")
+                .buildRooms("2rooms")
+                .build();
 
+        System.out.println(house);
     }
 }
