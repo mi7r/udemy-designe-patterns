e4c0e5af9e89462d2bd28394f3b4454fc1de0b75
Radek <michalski.rados@gmail.com>
2019-02-01 12:24:06 +0100

    Initial commit for factory

--- src/main/java/patterns/Main.java
+++ src/main/java/patterns/Main.java
@@ -1,7 +1,12 @@
 package patterns;
 
+import patterns.units.Rifleman;
+import patterns.units.Tank;
+import patterns.units.Unit;
+
 public class Main {
     public static void main(String[] args) {
-
+        Unit tank = new Tank(2000, 0, 50);
+        Unit rifleman = new Rifleman(200, 1,15);
     }
 }
---
+++ src/main/java/patterns/units/Rifleman.java
@@ -0,0 +1,7 @@
+package patterns.units;
+
+public class Rifleman extends Unit {
+    public Rifleman(int hp, int exp, int dmgDone) {
+        super(hp, exp, dmgDone);
+    }
+}
