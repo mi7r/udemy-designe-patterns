73f9581d028e73db77ba161b28e8a24adfc3d0e5
Radek <michalski.rados@gmail.com>
2019-02-01 11:26:21 +0100

    Initial commit for task builder.

---
+++ src/main/java/patterns/flight/FlightLeg.java
@@ -0,0 +1,8 @@
+package patterns.flight;
+
+public class FlightLeg {
+    private String flightFrom;
+    private String flightTo;
+    private boolean delayed = false;
+    private double price;
+}
