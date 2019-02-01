0af71e673740a855c0184d65b7e229ca0a195404
Radek <michalski.rados@gmail.com>
2019-02-01 12:06:10 +0100

    Task finished.

--- src/main/java/patterns/Main.java
+++ src/main/java/patterns/Main.java
@@ -1,7 +1,24 @@
 package patterns;
 
+import patterns.flight.FlightLeg;
+
 public class Main {
     public static void main(String[] args) {
+        FlightLeg flightLeg = new FlightLeg.FlightLegBuilder()
+                .setFlightStart("Gdansk")
+                .setFlightEnd("Wrocław")
+                .setFlightPrice(499.90)
+                .build();
 
+        System.out.println(flightLeg);
+
+        FlightLeg flightLeg1 = new FlightLeg.FlightLegBuilder()
+                .setFlightStart("Wrocław")
+                .setFlightEnd("Gdańsk")
+                .setFlightPrice(99.99)
+                .setDelayedStatus(true)
+                .build();
+
+        System.out.println(flightLeg1);
     }
 }
--- src/main/java/patterns/flight/FlightLeg.java
+++ src/main/java/patterns/flight/FlightLeg.java
@@ -1,8 +1,58 @@
 package patterns.flight;
 
 public class FlightLeg {
-    private String flightFrom;
-    private String flightTo;
-    private boolean delayed = false;
+    private String flightStart;
+    private String flightEnd;
+    private boolean delayed;
     private double price;
+
+    private FlightLeg(FlightLegBuilder flightLegBuilder) {
+        this.flightStart = flightLegBuilder.flightStart;
+        this.flightEnd = flightLegBuilder.flightEnd;
+        this.delayed = flightLegBuilder.delayed;
+        this.price = flightLegBuilder.price;
+    }
+
+    @Override
+    public String toString() {
+        return "FlightLeg{" +
+                "flightStart='" + flightStart + '\'' +
+                ", flightEnd='" + flightEnd + '\'' +
+                ", delayed=" + delayed +
+                ", price=" + price +
+                '}';
+    }
+
+    public static class FlightLegBuilder {
+
+        private String flightStart;
+        private String flightEnd;
+        private boolean delayed = false;
+        private double price;
+
+        public FlightLegBuilder setFlightStart(String flightFrom) {
+            this.flightStart = flightFrom;
+            return this;
+        }
+
+        public FlightLegBuilder setFlightEnd(String flightEnd) {
+            this.flightEnd = flightEnd;
+            return this;
+        }
+
+        public FlightLegBuilder setDelayedStatus(boolean delayed){
+            this.delayed = delayed;
+            return this;
+        }
+
+        public FlightLegBuilder setFlightPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+        public FlightLeg build(){
+            return new FlightLeg(this);
+        }
+
+    }
 }
